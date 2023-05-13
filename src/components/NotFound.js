a8b4a0c71cc6f0592faa134efb932de75b08a22b
Rick Miranda <dev.rickmiranda@gmail.com>
2023-05-13 16:04:04 -0300

    first commit edition

---
+++ src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NotFound = () => (
+<>
+  <h1>Error 404 - Página Não Encontrada!</h1>
+  <p>
+    <Link to={"/"}>Retornar para Tabela de Livros</Link>
+  </p>
+</>
+
+)
+
+
+export default NotFound
